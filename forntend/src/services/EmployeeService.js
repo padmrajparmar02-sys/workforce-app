3e5379f28812f36b5c3954a0d479de15a5a9a992
padmraj <padmrajparmar02@gmail.com>
2026-07-27 20:19:31 +0530

    config: pointing frontend service to live backend url

--- forntend/src/services/EmployeeService.js
+++ forntend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "https://workforce-app-production-65c9.up.railway.app/api/v1/employees";
 
 class EmployeeService {
 
